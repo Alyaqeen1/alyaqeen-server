780a2276adb3f2972441cb91050e7e9dbebd74f3
Sadia492 <sadiaafrin2792>
2025-05-17 13:33:53 +0600

    170525 users and students data added

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const cors = require("cors");
 const port = process.env.PORT || 9000;
 const app = express();
 
-app.use(cors());
+//Must remove "/" from your production URL
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://alyaqeen.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+//localhost:5000 and localhost:5173 are treated as same site.  so sameSite value must be strict in development server.  in production sameSite will be none
+// in development server secure will false .  in production secure will be true
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.dr5qw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -22,6 +36,35 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // collections
+    const usersCollection = client.db("alyaqeenDb").collection("users");
+    const studentsCollection = client.db("alyaqeenDb").collection("students");
+
+    // posting users here
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // posting students whole data here
+    app.post("/students", async (req, res) => {
+      const newStudent = req.body;
+      const result = await studentsCollection.insertOne(newStudent);
+      res.send(result);
+    });
+
+    // getting users here
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    // getting students here
+    app.get("/students", async (req, res) => {
+      const result = await studentsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
