dd08ac44a1a47487436c5e9eac7a56d54b05afe7
Sadia492 <sadiaafrin2792>
2025-08-14 20:21:11 +0600

    save

--- config/sessionMap.js
+++ config/sessionMap.js
@@ -2,7 +2,7 @@ const sessionMap = {
   S1: "Early - 4:30 PM – 6:00 PM (1½ hrs)",
   S2: "Late - 5:45 PM – 7:15 PM (1½ hrs)",
   WM: "Morning - 10:00 AM – 12:30 PM (2½ hrs)",
-  WA: "Afternoon - 12:30 PM – 2:30 PM (2½ hrs)",
+  WA: "Afternoon - 12:30 PM – 2:30 PM (2 hrs)",
 };
 
 module.exports = sessionMap;
